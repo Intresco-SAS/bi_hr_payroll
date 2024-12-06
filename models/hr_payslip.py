1e1e56475365f423137f40b59c08cd02ecf622fc
Nelson V <nvelez@intresco.co>
2024-12-06 23:38:41 +0000

    Corrección de función para capturar compañia

--- models/hr_payslip.py
+++ models/hr_payslip.py
@@ -44,7 +44,7 @@ class HrPayslip(models.Model):
     line_ids = fields.One2many('hr.payslip.line', 'slip_id', string='Payslip Lines', readonly=True,
         states={'draft': [('readonly', False)]})
     company_id = fields.Many2one('res.company', string='Company', readonly=True, copy=False,
-        default=lambda self: self.env['res.company']._company_default_get(),
+        default=lambda self: self.env.company,
         states={'draft': [('readonly', False)]})
     worked_days_line_ids = fields.One2many('hr.payslip.worked_days', 'payslip_id',
         string='Payslip Worked Days', copy=True, readonly=True,
--- models/hr_salary_rule.py
+++ models/hr_salary_rule.py
@@ -23,7 +23,7 @@ class HrPayrollStructure(models.Model):
     name = fields.Char(required=True)
     code = fields.Char(string='Reference', required=True)
     company_id = fields.Many2one('res.company', string='Company', required=True,
-        copy=False, default=lambda self: self.env['res.company']._company_default_get())
+        copy=False, default=lambda self: self.env.company)
     note = fields.Text(string='Description')
     parent_id = fields.Many2one('hr.payroll.structure', string='Parent', default=_get_parent)
     children_ids = fields.One2many('hr.payroll.structure', 'parent_id', string='Children', copy=True)
@@ -61,7 +61,7 @@ class HrContributionRegister(models.Model):
     _description = 'Contribution Register'
 
     company_id = fields.Many2one('res.company', string='Company',
-        default=lambda self: self.env['res.company']._company_default_get())
+        default=lambda self: self.env.company)
     partner_id = fields.Many2one('res.partner', string='Partner')
     name = fields.Char(required=True)
     register_line_ids = fields.One2many('hr.payslip.line', 'register_id',
@@ -80,7 +80,7 @@ class HrSalaryRuleCategory(models.Model):
     children_ids = fields.One2many('hr.salary.rule.category', 'parent_id', string='Children')
     note = fields.Text(string='Description')
     company_id = fields.Many2one('res.company', string='Company',
-        default=lambda self: self.env['res.company']._company_default_get())
+        default=lambda self: self.env.company)
 
     @api.constrains('parent_id')
     def _check_parent_id(self):
@@ -111,7 +111,7 @@ class HrSalaryRule(models.Model):
         help="Used to display the salary rule on payslip.")
     parent_rule_id = fields.Many2one('hr.salary.rule', string='Parent Salary Rule', index=True)
     company_id = fields.Many2one('res.company', string='Company',
-        default=lambda self: self.env['res.company']._company_default_get())
+        default=lambda self: self.env.company)
     condition_select = fields.Selection([
         ('none', 'Always True'),
         ('range', 'Range'),
